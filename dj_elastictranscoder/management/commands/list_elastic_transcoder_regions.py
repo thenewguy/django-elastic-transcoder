33ac4ffda2f4441d5c8c2133795a7190aba6c65d
gordon <wgordonw1@gmail.com>
2014-05-18 19:33:34 -0400

    these files are no longer needed now that valid regions are output as needed

--- dj_elastictranscoder/management/commands/list_elastic_transcoder_regions.py
+++
@@ -1,9 +0,0 @@
-from boto import elastictranscoder
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = 'Lists valid elastic transcoder regions.'
-
-    def handle(self, *args, **kwargs):
-        for region in elastictranscoder.regions():
-            self.stdout.write('%s' % region.name)

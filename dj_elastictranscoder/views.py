a9f4f85fce5093f3e1c06e381187afe2012491ec
gordon <wgordonw1@gmail.com>
2014-07-12 11:28:55 -0400

    wrap view in a try block and log any exceptions that are raised for debugging purposes

--- dj_elastictranscoder/views.py
+++ dj_elastictranscoder/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
@@ -12,61 +13,66 @@ from .signals import (
     transcode_oncomplete
 )
 
+logger = logging.getLogger("dj_elastictranscoder.views")
+
 @csrf_exempt
 def endpoint(request):
     """
     Receive SNS notification
     """
-
     try:
-        data = json.loads(request.read())
-    except ValueError:
-        return HttpResponseBadRequest('Invalid JSON')
-
-
-    # handle SNS subscription
-    if data['Type'] == 'SubscriptionConfirmation':
-        subscribe_url = data['SubscribeURL']
-        
-        subscribe_body = """
-        This message serves as a fail-safe in case the automatic subscription confirmation fails. 
-        
-        Please visit this URL below to confirm your subscription with SNS
-
-        %s """ % subscribe_url
-
-        mail_admins('Please confirm SNS subscription', subscribe_body)
-        urlopen(subscribe_url)
-        return HttpResponse('OK')
-
+        try:
+            data = json.loads(request.read())
+        except ValueError:
+            return HttpResponseBadRequest('Invalid JSON')
     
-    #
-    try:
-        message = json.loads(data['Message'])
-    except ValueError:
-        assert False, data['Message']
-
-    #
-    if message['state'] == 'PROGRESSING':
-        job = EncodeJob.objects.get(pk=message['jobId'])
-        job.message = 'Progress'
-        job.state = 1
-        job.save()
-
-        transcode_onprogress.send(sender=None, job=job, message=message)
-    elif message['state'] == 'COMPLETED':
-        job = EncodeJob.objects.get(pk=message['jobId'])
-        job.message = 'Success'
-        job.state = 4
-        job.save()
-
-        transcode_oncomplete.send(sender=None, job=job, message=message)
-    elif message['state'] == 'ERROR':
-        job = EncodeJob.objects.get(pk=message['jobId'])
-        job.message = message['messageDetails']
-        job.state = 2
-        job.save()
-
-        transcode_onerror.send(sender=None, job=job, message=message)
-
-    return HttpResponse('Done')
+    
+        # handle SNS subscription
+        if data['Type'] == 'SubscriptionConfirmation':
+            subscribe_url = data['SubscribeURL']
+            
+            subscribe_body = """
+            This message serves as a fail-safe in case the automatic subscription confirmation fails. 
+            
+            Please visit this URL below to confirm your subscription with SNS
+    
+            %s """ % subscribe_url
+    
+            mail_admins('Please confirm SNS subscription', subscribe_body)
+            urlopen(subscribe_url)
+            return HttpResponse('OK')
+    
+        
+        #
+        try:
+            message = json.loads(data['Message'])
+        except ValueError:
+            assert False, data['Message']
+    
+        #
+        if message['state'] == 'PROGRESSING':
+            job = EncodeJob.objects.get(pk=message['jobId'])
+            job.message = 'Progress'
+            job.state = 1
+            job.save()
+    
+            transcode_onprogress.send(sender=None, job=job, message=message)
+        elif message['state'] == 'COMPLETED':
+            job = EncodeJob.objects.get(pk=message['jobId'])
+            job.message = 'Success'
+            job.state = 4
+            job.save()
+    
+            transcode_oncomplete.send(sender=None, job=job, message=message)
+        elif message['state'] == 'ERROR':
+            job = EncodeJob.objects.get(pk=message['jobId'])
+            job.message = message['messageDetails']
+            job.state = 2
+            job.save()
+    
+            transcode_onerror.send(sender=None, job=job, message=message)
+    
+        return HttpResponse('Done')
+    except Exception, e:
+        logger.exception("'%s' exception was raised processing the endpoint view." % e.__class__.__name__)
+        raise

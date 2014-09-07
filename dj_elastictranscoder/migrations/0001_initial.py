ab4cf43c51d270cecddb96f7fa4fff21ff83e620
gordon <wgordonw1@gmail.com>
2014-09-07 18:03:59 -0400

    fix initial migration for released django 1.7

--- dj_elastictranscoder/migrations/0001_initial.py
+++ dj_elastictranscoder/migrations/0001_initial.py
@@ -1,4 +1,4 @@
-# encoding: utf8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
@@ -15,12 +15,12 @@ class Migration(migrations.Migration):
             name='EncodeJob',
             fields=[
                 ('id', models.CharField(max_length=100, serialize=False, primary_key=True)),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType', to_field='id')),
                 ('object_id', models.PositiveIntegerField()),
-                ('state', models.PositiveIntegerField(default=0, db_index=True, choices=[(0, b'Submitted'), (1, b'Progressing'), (2, b'Error'), (3, b'Warning'), (4, b'Complete')])),
+                ('state', models.PositiveIntegerField(default=0, db_index=True, choices=[(0, b'Submitted'), (1, b'Progressing'), (2, b'Error'), (3, b'Complete')])),
                 ('message', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('last_modified', models.DateTimeField(auto_now=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
             ],
             options={
             },
